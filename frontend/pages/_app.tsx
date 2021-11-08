9d806afc73cc2d781cfaefd6bcbd6027de8efd84
adriandelgg <adriandelgadoc1@gmail.com>
2021-11-07 20:50:58 -0800

    Fixed and updated packages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,22 +1,22 @@
 import type { AppProps } from 'next/app';
-import '@styles/globals.css';
+import 'styles/globals.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Web3Provider from 'context/Web3Context';
 import Layout from 'context/Layout';
-import { store } from '@redux/store';
-import { Provider } from 'react-redux';
+// import { store } from '@redux/store';
+// import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Provider store={store}>
-			<Web3Provider>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</Web3Provider>
-		</Provider>
+		// <Provider store={store}>
+		<Web3Provider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Web3Provider>
+		// </Provider>
 	);
 }
 
